9d459da358591773fa161cebad09d6315d495fab
Levi <liwei2@che001.com>
2017-09-30 09:58:36 +0800

    'koaviews'

--- server.js
+++ server.js
@@ -4,21 +4,37 @@
 import koa from 'koa'
 import Router from 'koa-router'
 import bodyParser from 'koa-bodyparser'
+import views from 'koa-views'
+import path from 'path'
 let app=new koa();
+let router=new Router();
 app.use(bodyParser({
-    strict:false,
-    extendTypes: {
-        json: ['application/x-javascript'] // will parse application/x-javascript type body as a JSON string
-    },
     onerror: function (err, ctx) {
         ctx.throw(err);
     }
 }));
-app.use(async (ctx)=>{
-    console.log('get:%j',ctx.request.query)
-    console.log('post:%j',ctx.request.body);
-    ctx.body="hello koa2";
+
+app.use(views(path.join(__dirname,'./views'),{
+    extension:'hbs',
+    map: { hbs: 'handlebars' },
+}));
+app.use(async (ctx,next)=>{
+    ctx.state={name:'123',age:'222'}
+  await next();
 });
+router.get('/',async (ctx)=>{
+    ctx.state = { title: '111', author: 'queckezz' }
+    return ctx.render('index')
+});
+router.get('/hello',async (ctx)=>{
+    console.log(ctx.state)
+    return ctx.render('hello')
+});
+app.use(router.routes(),router.allowedMethods());
+/*app.use(async (ctx)=>{
+    ctx.state = { title: '111', author: 'queckezz' }
+    //return ctx.render('index.hbs',{name:'战三',age:'zhangsan'})
+});*/
 app.listen(8000);
 app.on('error',(err,ctx)=>{
    console.log(err);
